67d45839d8c17f96c2e59be1de70ddf1bb0e3496
Afonso <jedipadawanafonso@gmail.com>
2020-05-20 13:11:23 +0100

    login

--- login.php
+++ login.php
@@ -86,7 +86,7 @@
                 <div id="login-column" class="col-md-6">
                     <div id="login-box" class="col-md-12">
                         <form id="login-form" class="form" action="" method="post">
-                            <h3 class="text-center text-info">Login</h3>
+                        <img src="images/sign_in.png" class="signin" style="width: 25px">
                             <div class="form-group">
                                 <label for="username" class="text-info">Username:</label><br>
                                 <input type="text" name="username" id="username" class="form-control">
