9db65b842e526eb4e58882645b4fedc2eaaaa806
Afonso <jedipadawanafonso@gmail.com>
2020-05-25 09:09:06 +0100

    updated footer

--- contactos.php
+++ contactos.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -24,61 +24,11 @@
     <script src="scripts/js/bootstrap.min.js"></script>
 	<script src="scripts/js/bootstrap.bundle.min.js"></script>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="index.php"><img src="images/dotlog_logo.png" class="logo" style="width: 200px"></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        
-        
-      </ul>
+  <?php
+  require_once "navbar.php";
+  require "connectdb.php";
 
-      <form class="form-inline my-2 my-lg-0">
-        <ul class="navbar-nav mr-auto opcao">
-          <li class="nav-item opcao">
-            <a class="nav-link" href="sobre.html">Sobre</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link opcao" href="contactos.html">Contactos</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Produtos
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item opcao" href="#">Hardware</a>
-			  <div class="dropdown-divider"></div>
-              <a class="dropdown-item opcao" href="#">Software</a>
-			  <div class="dropdown-divider"></div>
-              <a class="dropdown-item opcao" href="#">Consumiveis</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link opcao" href="servicos.html">Serviços</a>
-          </li>
-		  <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <img src="images/sign_in.png" class="signin" style="width: 25px">
-            </a>
-            <div class="dropdown-menu dropdown-menu-right text-muted" aria-labelledby="navbarDropdown">
-				<!--<p style="border-block-end: 0px">António Silva</p>
-				<p style="border-block-end: 0px">SilvaInc</p>-->
-				<p style="justify-content:normal;color:rgb(54, 54, 54);padding:.25rem 1.5rem">
-					António Silva
-					<br>
-					SilvaInc
-				</p>
-				<div class="dropdown-divider"></div>
-              <a class="dropdown-item opcao" href="#">Sign Out</a>
-              
-          </li>
-		  
-        </ul>
-      </form>
-    </div>
-  </nav>
+  ?>
 
     <table border="0" style="width:50%;border-collapse:collapse">
       <tr>
@@ -134,55 +84,10 @@
       </tr>
     </table>
 
-    <footer class="page-footer font-small unique-color-dark footer" style="background-color: #30373f;"><br>
-    <div class="container text-center text-md-left mt-5">
+    <?php
+  require_once "footer.php";
 
-      <div class="row mt-3">
+  ?>
 
-        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-
-          <h6 class="text-uppercase font-weight-bold">Dotlog</h6>
-          <hr class="white mb-4 mt-0 d-inline-block mx-auto" style="width: 75px;border-top: 1px solid rgb(255,255,255)">
-          <p>lorem ipsum</p>
-
-        </div>
-
-        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-
-          <h6 class="text-uppercase font-weight-bold">Contactos</h6>
-          <hr class="white mb-4 mt-0 d-inline-block mx-auto" style="width: 110px;border-top: 1px solid rgb(255,255,255)">
-          <p>Leiria</p>
-          <p>dotlog@gmail.com</p>
-          <p>923 424 112</p>
-          <p>08:00 - 21:00</p>
-        </div>
-
-        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-
-          <h6 class="text-uppercase font-weight-bold">Links Rapidos</h6>
-          <hr class="white mb-4 mt-0 d-inline-block mx-auto" style="width: 150px;border-top: 1px solid rgb(255,255,255)">
-          <p>
-            <a href="#!">Home</a>
-          </p>
-          <p>
-            <a href="#!">Serviços</a>
-          </p>
-          <p>
-            <a href="#!">Sobre</a>
-          </p>
-          <p>
-            <a href="#!">Contactos</a>
-          </p>
-
-        </div>
-
-      </div>
-
-	  
-	  <div class="footer-copyright text-center font-weight-bold" style="color: white;font-size: 11t;">© 2020 DotLog
-    </div>
-    </div>
-    
-  </footer>
   </body>
 </html>
